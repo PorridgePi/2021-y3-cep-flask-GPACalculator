4280cbd6305c1692dfc4c8a42c948d7dfe00c46c
Yikun Zhou <>
2021-05-20 00:43:52 +0000

    feat: Add routes for /testing and 404 error

--- main.py
+++ main.py
@@ -6,5 +6,13 @@ app = flask.Flask(__name__)
 @app.route("/")
 def root():
   return render_template("index.html")
-  
+
+@app.route("/testing")
+def testing():
+  return render_template("testing.html")
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
 app.run(host="0.0.0.0", port=8080, debug=True)
