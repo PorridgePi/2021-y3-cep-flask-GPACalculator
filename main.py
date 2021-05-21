f7c7e33094209d76220b9b9b2a4a7c6aa1e3e7d0
Yikun Zhou <>
2021-05-21 14:10:19 +0000

    chore: Standardise indentation to 4 spaces

--- main.py
+++ main.py
@@ -5,23 +5,22 @@ app = flask.Flask(__name__)
 
 @app.route("/")
 def root():
-  return render_template("index.html")
+    return render_template("index.html")
 
 @app.route("/testing")
 def testing():
-  return render_template("testing.html")
+    return render_template("testing.html")
 
 @app.route("/form")
 def form():
-  return render_template("form.html")
-
+    return render_template("form.html")
 
 @app.route("/calculate")
 def calculate():
-  output = "Mark 1: " + request.args["mark1"] + ", weightage 1: " + request.args["weightage1"] + "<br>"
-  output += "Mark 2: " + request.args["mark2"] + ", weightage 2: " + request.args["weightage2"] + "<br>"
-  output += "Mark 3: " + request.args["mark3"] + ", weightage 3: " + request.args["weightage3"] + "<br>"
-  return output
+    output = "Mark 1: " + request.args["mark1"] + ", weightage 1: " + request.args["weightage1"] + "<br>"
+    output += "Mark 2: " + request.args["mark2"] + ", weightage 2: " + request.args["weightage2"] + "<br>"
+    output += "Mark 3: " + request.args["mark3"] + ", weightage 3: " + request.args["weightage3"] + "<br>"
+    return output
 
 @app.errorhandler(404)
 def page_not_found(error):
