ee42b74f01e86db1121bbb6f9f9803396e0b3ee9
Yikun Zhou <>
2021-05-21 14:06:15 +0000

    feat: Add mark and weightage entry form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import flask
-from flask import render_template
+from flask import render_template, request
 
 app = flask.Flask(__name__)
 
@@ -10,6 +10,18 @@ def root():
 @app.route("/testing")
 def testing():
   return render_template("testing.html")
+
+@app.route("/form")
+def form():
+  return render_template("form.html")
+
+
+@app.route("/calculate")
+def calculate():
+  output = "Mark 1: " + request.args["mark1"] + ", weightage 1: " + request.args["weightage1"] + "<br>"
+  output += "Mark 2: " + request.args["mark2"] + ", weightage 2: " + request.args["weightage2"] + "<br>"
+  output += "Mark 3: " + request.args["mark3"] + ", weightage 3: " + request.args["weightage3"] + "<br>"
+  return output
 
 @app.errorhandler(404)
 def page_not_found(error):
