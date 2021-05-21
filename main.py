dcdae830002cc31af75ec467e402bbae3fabc326
Yikun Zhou <>
2021-05-21 14:48:02 +0000

    feat: Add percentage and GPA calulation

--- main.py
+++ main.py
@@ -17,9 +17,42 @@ def form():
 
 @app.route("/calculate")
 def calculate():
-    output = "Mark 1: " + request.args["mark1"] + ", weightage 1: " + request.args["weightage1"] + "<br>"
-    output += "Mark 2: " + request.args["mark2"] + ", weightage 2: " + request.args["weightage2"] + "<br>"
-    output += "Mark 3: " + request.args["mark3"] + ", weightage 3: " + request.args["weightage3"] + "<br>"
+    mark1, mark2, mark3 = float(request.args["mark1"]), float(request.args["mark2"]), float(request.args["mark3"])
+
+    weightage1, weightage2, weightage3 = float(request.args["weightage1"]), float(request.args["weightage2"]), float(request.args["weightage3"])
+
+    totalWeightage = weightage1 + weightage2 + weightage3
+
+    rawPercentage = (mark1 / 100 / totalWeightage * weightage1 + mark2 / 100 / totalWeightage * weightage2 + mark3 / 100 / totalWeightage * weightage3) * 100
+
+    totalPercentage = round(rawPercentage, 1)
+
+    if totalPercentage >= 80:
+        grade = "4.0"
+    elif totalPercentage >= 70:
+        grade = "3.6"
+    elif totalPercentage >= 65:
+        grade = "3.2"
+    elif totalPercentage >= 60:
+        grade = "2.8"
+    elif totalPercentage >= 55:
+        grade = "2.4"
+    elif totalPercentage >= 50:
+        grade = "2.0"
+    elif totalPercentage >= 45:
+        grade = "1.6"
+    elif totalPercentage >= 40:
+        grade = "1.2"
+    else:
+        grade = "0.8"
+
+    output = "Mark 1: " + str(mark1) + " Weightage 1: " + str(weightage1) + "<br>"
+    output += "Mark 2: " + str(mark2) + " Weightage 2: " + str(weightage2) + "<br>"
+    output += "Mark 3: " + str(mark3) + " Weightage 3: " + str(weightage3) + "<br>"
+
+    output += "Percentage: " + str(totalPercentage) + "<br>"
+    output += "GPA: " + grade
+
     return output
 
 @app.errorhandler(404)
